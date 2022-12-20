c570394f2a4002326b96d625525fd6eb522a0e55
ybs <yubenshu@yeah.net>
2022-12-20 16:57:45 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author ybs
+ * @create 2022-12-20 16:54
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
